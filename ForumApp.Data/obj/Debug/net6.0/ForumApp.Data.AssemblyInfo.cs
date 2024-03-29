0c6368da09c6503835e04fdbe70fbf6cb511fd33
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-29 09:41:11 +0200

    Update ForumApp.Data.AssemblyInfo.cs

--- ForumApp.Data/obj/Debug/net6.0/ForumApp.Data.AssemblyInfo.cs
+++ ForumApp.Data/obj/Debug/net6.0/ForumApp.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForumApp.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bddaf1e8a73f7b535395c4f5a6bd4743b3267f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e3792a64f31d4f2382da99ccfc5b6cf4bbc8e24")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForumApp.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForumApp.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
